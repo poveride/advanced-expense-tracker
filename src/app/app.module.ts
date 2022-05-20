cdf10f361c9f38197ae227fe4bedb83fe3a4e09f
poveride <poveride@gmail.com>
2022-05-20 21:45:18 +0200

    buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButonComponent } from './components/buton/buton.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/buton/buton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buton',
+  templateUrl: './buton.component.html',
+  styleUrls: ['./buton.component.scss']
+})
+export class ButonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
